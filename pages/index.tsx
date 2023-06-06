86bc0148a128cd5781705aae2718057b71ebbe4f
ma9pie <ma9pie.dev@gmail.com>
2023-06-07 02:38:46 +0900

    feat : index 페이지 링크 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Button from "@/components/Button";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 
 const Page = () => {
-  return <div>index</div>;
+  const router = useRouter();
+  return (
+    <Wrapper>
+      <Container>
+        <Button onClick={() => router.push("/useState")}>
+          useState로 구현하기
+        </Button>
+        <Button onClick={() => router.push("/createRoot")}>
+          createRoot로 구현하기
+        </Button>
+        <Button onClick={() => router.push("/redux")}>Redux로 구현하기</Button>
+        <Button onClick={() => router.push("/recoil")}>
+          Recoil로 구현하기
+        </Button>
+        <Button onClick={() => router.push("/context")}>
+          Context로 구현하기
+        </Button>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Page;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Container = styled.div`
+  display: grid;
+  gap: 16px;
+`;
